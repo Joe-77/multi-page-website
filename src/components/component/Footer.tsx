1eb591fd7f12572c07a91d92d9d9462fa98e6ffe
Yousef <yousefabdallah55464@gmail.com>
2024-01-18 09:48:15 +0200

    update footer

--- src/components/component/Footer.tsx
+++ src/components/component/Footer.tsx
@@ -6,13 +6,13 @@ import Msg from "./Msg";
 const Footer = () => {
   return (
     <footer className="bg-black mt-40 text-white pt-7 relative px-4 lg:px-0">
-      <Msg/>
+      <Msg />
       <div className="container m-auto px-4 sm:px-0 pt-28 pb-5 sm:pt-14">
         <div className="flex items-center justify-between pb-5 border-b-[1px] border-b-gray-600">
           <Link to="/">
-            <img className="w-28 sm:w-40" src={logo} alt="" />
+            <img className="w-20 sm:w-40" src={logo} alt="" />
           </Link>
-          <ul className="flex gap-4 text-[11px] sm:text-sm">
+          <ul className="flex gap-4 text-[9px] sm:text-sm">
             <li>
               <Link to="/about">our company</Link>
             </li>
@@ -24,30 +24,30 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="flex items-end justify-between mt-5 pb-5">
+        <div className="grid grid-cols-3 gap-2 flex-wrap items-end justify-between mt-5 pb-5">
           <div>
-            <p className="text-[10px] sm:text-xs leading-6 text-gray-500">
-              Designo Central Office <br /> 3886 Wellington Street
-              <br /> Toronto, Ontario M9C 3J5
+            <p className="text-[7px] sm:text-xs leading-6 text-gray-500">
+              Designo Central Office 3886 Wellington Street Toronto, Ontario M9C
+              3J5
             </p>
           </div>
-          <div className="text-[10px] sm:text-xs flex flex-col gap-2 text-gray-500 mb-2">
+          <div className="text-[8px] sm:text-xs flex flex-col gap-2 text-gray-500 mb-2">
             <p>Contact Us (Central Office)</p>
-            <p>P : +1 253-863-8967</p>
+            <p >P : +1 253-863-8967</p>
             <p>M : contact@designo.co</p>
           </div>
 
-          <div className="flex items-center gap-3 mb-2">
-            <span className="text-lg sm:text-2xl text-orange-500 duration-500 hover:text-orange-800 w-fit cursor-pointer">
+          <div className="flex items-center gap-3 mb-2 ">
+            <span className="text-xs sm:text-2xl text-orange-500 duration-500 hover:text-orange-800 w-fit cursor-pointer">
               <FaFacebookF />
             </span>
-            <span className="text-lg sm:text-2xl text-orange-500 duration-500 hover:text-orange-800 w-fit cursor-pointer">
+            <span className="text-xs sm:text-2xl text-orange-500 duration-500 hover:text-orange-800 w-fit cursor-pointer">
               <FaYoutube />
             </span>
-            <span className="text-lg sm:text-2xl text-orange-500 duration-500 hover:text-orange-800 w-fit cursor-pointer">
+            <span className="text-xs sm:text-2xl text-orange-500 duration-500 hover:text-orange-800 w-fit cursor-pointer">
               <FaTwitter />
             </span>
-            <span className="text-lg sm:text-2xl text-orange-500 duration-500 hover:text-orange-800 w-fit cursor-pointer">
+            <span className="text-xs sm:text-2xl text-orange-500 duration-500 hover:text-orange-800 w-fit cursor-pointer">
               <FaInstagram />
             </span>
           </div>
--- src/components/component/Msg.tsx
+++ src/components/component/Msg.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Msg = () => {
   return (
-    <div className="absolute w-3/4 lg:w-1/2 left-1/2 top-[-120px] px-10 lg:px-4 py-5 rounded-lg -translate-x-1/2 bg-primary">
+    <div className="absolute w-3/4 lg:w-1/2 left-1/2 top-[-120px] px-1 lg:px-4 py-5 rounded-lg -translate-x-1/2 bg-primary">
       <div className="absolute bg-circle w-3/4 lg:w-1/2 h-full bg-cover bg-no-repeat"></div>
       <div className="relative z-50 flex gap-4 flex-col sm:flex-row items-center justify-center sm:justify-between">
         <div className="w-60 m-auto sm:m-0 text-center sm:text-start">
-          <h2 className="text-xl font-mono bold">
+          <h2 className="text-sm sm:text-xl font-mono bold">
             Let’s talk about your project
           </h2>
           <p className="mt-3 text-xs leading-5 opacity-60">
